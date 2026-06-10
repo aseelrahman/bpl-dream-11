470025a1aa47595a69ea486783936e546b7cba20
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-06-10 09:28:02 -0400

    state management and toggle between available and selected players

--- src/Components/HomePage/Players/AvailablePlayers/AvailablePlayers.jsx
+++ src/Components/HomePage/Players/AvailablePlayers/AvailablePlayers.jsx
@@ -1,13 +1,11 @@
 import Card from "../../../Ui/Card";
 
 const AvailablePlayers = ({ players }) => {
-  console.log(players);
 
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {players.map((player) => {
-          console.log(player);
 
           return (
             <>
--- src/Components/HomePage/Players/Players.jsx
+++ src/Components/HomePage/Players/Players.jsx
@@ -1,14 +1,36 @@
-import { use } from 'react';
-import AvailablePlayers from './AvailablePlayers/AvailablePlayers';
+import { use, useState } from "react";
+import AvailablePlayers from "./AvailablePlayers/AvailablePlayers";
+import SelectedPlayers from "../SelectedPlayers/SelectedPlayers";
 
-const Players = ({playersPromise}) => {
-    const players = use(playersPromise)
-    return (
-        <div className='container mx-auto'>
-            Players: {players.length}
-            <AvailablePlayers players={players} />
+const Players = ({ playersPromise }) => {
+  const players = use(playersPromise);
+  const [selectedType, setSelectedType] = useState("available");
+  return (
+    <div className="container mx-auto my-15">
+      <div className="flex justify-between gap-4 items-center mb-5">
+        <h2 className="font-bold text-3xl">{selectedType === "available" ? "Available Players" : "Selected Players (2/6)"}</h2>
+        <div>
+          <button
+            onClick={() => setSelectedType("available")}
+            className={`btn ${selectedType === "available" ? "bg-[#E7FE29]" : "btn-neutral"} rounded-r-none rounded-l-xl`}
+          >
+            Available
+          </button>
+          <button
+            onClick={() => setSelectedType("selected")}
+            className={`btn ${selectedType === "selected" ? "bg-[#E7FE29]" : "btn-neutral"} rounded-l-none rounded-r-xl`}
+          >
+            Selected (0)
+          </button>
+
         </div>
-    );
+      </div>
+      {
+        selectedType === 'available'? <AvailablePlayers players={players} /> : <SelectedPlayers />
+      }
+      
+    </div>
+  );
 };
 
 export default Players;
